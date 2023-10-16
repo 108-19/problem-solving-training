7e0208f2c44bd3375aea1c9838dc72901b7091b7
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-16 21:14:58 +0300

    Update 51A.cpp

--- atcoder/phase1.1/51A.cpp
+++ atcoder/phase1.1/51A.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-string s;
-cin>>s;
-for(int i=0;i<s.size();i++){
-    if(s[i]==','){
-        cout<<" ";
+    string s;
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+        if(s[i]==','){
+            cout<<" ";
+        }
+        else {
+            cout<<s[i];
+        }
     }
-    else {
-        cout<<s[i];
-    }
-}
-return 0;
+    return 0;
 }
