6a0f49b0e44d98cae000b97ce5909eb33744f701
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-22 01:48:44 +0300

    Create 57A.cpp

---
+++ atcoder/phase1.1/57A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main() {
+    Martina
+        int a,b,c; cin>>a>>b;
+        if(a+b<=23){
+          cout<<a+b;
+      }
+      else {
+          cout<<a+b-24;
+      }
+        return 0;
+    }
