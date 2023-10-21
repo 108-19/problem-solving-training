b23227baf69892862e200be55636b62086e19123
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-22 00:50:43 +0300

    Create 53A.cpp

---
+++ atcoder/phase1.1/53A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main() {
+    Martina
+      int x; cin>>x;
+          if(x<1200){
+              cout<<"ABC";
+          }
+          else {
+              cout<<"ARC";
+          }
+          return 0;
+        }
