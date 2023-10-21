6dd4cf1e36b3180f77e31e90ac5dde93d6333e6d
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-22 01:39:45 +0300

    Create 56A.cpp

---
+++ atcoder/phase1.1/56A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main() {
+    Martina
+       char a,b; cin>>a>>b;
+           if(a==b){
+               cout<<"H";
+           }
+           else if(a!=b){
+               cout<<"D";
+           }
+               return 0;
+        }
