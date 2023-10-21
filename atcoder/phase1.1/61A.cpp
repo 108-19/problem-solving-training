081f3b8e1d2f470a0853efb51c5dd6371411a277
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-22 02:58:06 +0300

    Create 61A.cpp

---
+++ atcoder/phase1.1/61A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main() {
+    Martina
+        int a,b,c; cin>>a>>b>>c;
+        if(c>=a && c<=b){
+            cout<<"Yes";
+        }
+        else {
+            cout<<"No";
+        }
+
+
+        return 0;
+    }
