12e7a8093c422dda33f2c5d8abcbab76a98e3fc4
108-19 <martinasamuel10@gmail.com>
2023-10-22 22:29:25 +0300

    Update 88A.cpp

--- atcoder/phase1.1/2018-2019/88A.cpp
+++ atcoder/phase1.1/2018-2019/88A.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
-#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 int main() {
-  Martina
   int N,A;
   cin >> N >> A;
   if (N%500 <= A){
