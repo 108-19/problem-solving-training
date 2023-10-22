0bc1749077650e7dda636e9a604969ba872b377f
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-22 22:24:23 +0300

    Update 63A.cpp

--- atcoder/phase1.1/2016-2017/63A.cpp
+++ atcoder/phase1.1/2016-2017/63A.cpp
@@ -1,9 +1,8 @@
 #include <bits/stdc++.h>
-#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 int main() {
-    Martina
-        int a,b; cin>>a>>b;
+        int a,b; 
+        cin>>a>>b;
         if (a + b < 10) {
             cout << a + b;
         }
