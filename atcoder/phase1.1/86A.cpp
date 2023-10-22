ef10e421bed21f0d85c6ea75acc4933e5365c04c
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-22 06:42:17 +0300

    Create 86A.cpp

---
+++ atcoder/phase1.1/86A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main() {
+    Martina
+        int a,b,c; cin>>a>>b;
+        c=a*b;
+        if(c%2==0){
+            cout<<"Even";
+        }
+        else {
+            cout<<"Odd";
+        }
+          return 0;
+        }
