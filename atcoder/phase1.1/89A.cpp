d496536a676d4a780804267f08fd84976eb859f2
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-22 07:34:32 +0300

    Create 89A.cpp

---
+++ atcoder/phase1.1/89A.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main() {
+    Martina
+    int n; cin>>n;
+    cout<<n/3;
+      return 0;
+    }
