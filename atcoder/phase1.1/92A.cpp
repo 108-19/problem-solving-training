e452be0877609a410a79f7f40fc5568705b7389b
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-22 07:50:30 +0300

    Create 92A.cpp

---
+++ atcoder/phase1.1/92A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main() {
+    Martina
+    int a,b,c,d; cin>>a>>b>>c>>d;
+   int t=min(a,b)+min(c,d);
+   cout<<t;
+      return 0;
+    }
