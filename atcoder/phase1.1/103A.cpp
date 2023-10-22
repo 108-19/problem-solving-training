b186cabff1a1d4db2d86c2f9b9a75bb30f8fb207
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-22 06:02:56 +0300

    Create 103A.cpp

---
+++ atcoder/phase1.1/103A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main() {
+    Martina
+        int a1,a2,a3; cin>>a1>>a2>>a3;
+        int c=max({a1,a2,a3})-min({a1,a2,a3});
+        cout<<c;
+          return 0;
+        }
