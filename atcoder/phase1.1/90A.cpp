d6f639457b1d5efa80955af8858ea393e086d833
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-22 07:40:09 +0300

    Create 90A.cpp

---
+++ atcoder/phase1.1/90A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main() {
+    Martina
+    string s1,s2,s3; cin>>s1>>s2>>s3;
+    char a=s1[0];
+    char b=s2[1];
+    char c=s3[2];
+    cout<<a<<b<<c;
+      return 0;
+    }
