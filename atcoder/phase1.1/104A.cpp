4dd66d7e8cf48d85101fc8b8f8fb933577221ac2
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-22 05:41:22 +0300

    Create 104A.cpp

---
+++ atcoder/phase1.1/104A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main() {
+    Martina
+        int R;
+        cin >> R;
+        if (R < 1200) {
+            cout << "ABC";
+        }
+        else if (R < 2800) {
+            cout << "ARC";
+        }
+        else{
+            cout<<"AGC";
+        }
+          return 0;
+        }
