3c51fa165c4cd5ef504b96e591a77f631836fbde
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-22 07:29:23 +0300

    Create 88A.cpp

---
+++ atcoder/phase1.1/88A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main() {
+  Martina
+  int N,A;
+  cin >> N >> A;
+  if (N%500 <= A){
+    cout << "Yes" << endl;
+  }
+  else {
+    cout << "No" << endl;
+  }
+}
+return 0;
+}
