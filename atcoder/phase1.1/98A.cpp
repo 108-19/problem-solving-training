e68dab7c4d68b600ec4edde2effb9121f760f7d6
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-22 08:23:15 +0300

    Create 98A.cpp

---
+++ atcoder/phase1.1/98A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main() {
+    Martina
+        Martina
+        int a,b; cin>>a>>b;
+       int c=max({a+b,a-b,a*b});
+       cout<<c;
+
+    return 0;
+    }
