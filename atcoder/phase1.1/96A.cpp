ab3690af392cf2df155be1ec9e57240900f953b8
108-19 <125561036+108-19@users.noreply.github.com>
2023-10-22 08:12:40 +0300

    Create 96A.cpp

---
+++ atcoder/phase1.1/96A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main() {
+    Martina
+        Martina
+        int a,b,x;
+        cin>>a>>b>>x;
+
+        if(a<=x&&x<=a+b) {
+            cout<<"YES";
+        }
+        else {
+            cout<<"NO";
+        }
+
+    return 0;
+    }
