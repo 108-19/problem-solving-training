40c5fef990d605aaa7ba998b7e356817aeae2e25
108-19 <125561036+108-19@users.noreply.github.com>
2023-11-05 19:53:00 +0200

    Create 150A.cpp

---
+++ atcoder/phase1.1/150A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    martina
+    int k,x;
+    cin>>k>>x;
+    if(500*k >=x){
+        cout<<"Yes";
+    }
+    else {
+        cout<<"No";
+    }
+    return 0;
+}
