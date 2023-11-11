025c2710460bc0e08addf877a4dd01357a34eb25
108-19 <125561036+108-19@users.noreply.github.com>
2023-11-12 00:36:35 +0200

    53A.cpp

--- atcoder/phase1.1/2016-2017/53A.cpp
+++ atcoder/phase1.1/2016-2017/53A.cpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
-#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 int main() {
-    Martina
-      int x; cin>>x;
-          if(x<1200){
-              cout<<"ABC";
-          }
-          else {
-              cout<<"ARC";
-          }
-          return 0;
-        }
+    int x; cin>>x;
+    if(x<1200){
+        cout<<"ABC";
+    }
+    else {
+        cout<<"ARC";
+    }
+    return 0;
+}
