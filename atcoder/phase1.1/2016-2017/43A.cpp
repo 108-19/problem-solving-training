ca3a5483836f446c35c2f814edbd43921bec8e15
108-19 <125561036+108-19@users.noreply.github.com>
2023-11-12 00:14:13 +0200

    43A.cpp

--- atcoder/phase1.1/2016-2017/43A.cpp
+++ atcoder/phase1.1/2016-2017/43A.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-int n;
-cin>>n;
-int total;
-for(int i=0;i<=n;i++) {
-    total += i;
-}
-    cout<<total;
-
-return 0;
-}
+    int n;
+    cin>>n;
+    int total;
+    for(int i=0;i<=n;i++) {
+        total += i;
+    }
+        cout<<total;
+    return 0;
+    }
