c25cb76c3907a05ecf8794ea2c733af714378488
108-19 <125561036+108-19@users.noreply.github.com>
2023-11-12 01:03:59 +0200

    59A.cpp

--- atcoder/phase1.1/2016-2017/59A.cpp
+++ atcoder/phase1.1/2016-2017/59A.cpp
@@ -1,12 +1,9 @@
 #include <bits/stdc++.h>
 #include <algorithm>
-#include <string>
-#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 int main() {
-    Martina
     string s1, s2, s3;
     cin >> s1 >> s2 >> s3;
     cout<<char(s1[0]-32)<<char(s2[0]-32)<<char(s3[0]-32);
-      return 0;
-    }
+    return 0;
+}
