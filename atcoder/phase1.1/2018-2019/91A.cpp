8f5b899cd4cf8a9ae634c4a6516988b4b31fccc6
108-19 <125561036+108-19@users.noreply.github.com>
2023-11-12 01:34:44 +0200

    91A.cpp

--- atcoder/phase1.1/2018-2019/91A.cpp
+++ atcoder/phase1.1/2018-2019/91A.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
-#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 int main() {
-    Martina
-    int a,b,c; cin>>a>>b>>c;
+    int a,b,c; 
+    cin>>a>>b>>c;
     if (a+b>=c){
         cout<<"Yes";
     }
     else {
         cout<<"No";
     }
-      return 0;
-    }
+    return 0;
+}
