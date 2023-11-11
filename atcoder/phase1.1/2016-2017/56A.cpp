ae22823adde044bd16e9e42f6e5daba554da7120
108-19 <125561036+108-19@users.noreply.github.com>
2023-11-12 01:00:36 +0200

    56A.cpp

--- atcoder/phase1.1/2016-2017/56A.cpp
+++ atcoder/phase1.1/2016-2017/56A.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
-#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 int main() {
-    Martina
-       char a,b; cin>>a>>b;
-           if(a==b){
-               cout<<"H";
-           }
-           else if(a!=b){
-               cout<<"D";
-           }
-               return 0;
-        }
+    char a,b;
+    cin>>a>>b;
+    if(a==b){
+        cout<<"H";
+    }
+    else if(a!=b){
+        cout<<"D";
+    }
+    return 0;
+}
