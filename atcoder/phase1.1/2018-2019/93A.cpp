bf8390eceb24711f2c3547d915b85500be88f32f
108-19 <125561036+108-19@users.noreply.github.com>
2023-11-12 01:59:25 +0200

    93A.cpp

--- atcoder/phase1.1/2018-2019/93A.cpp
+++ atcoder/phase1.1/2018-2019/93A.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
-#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 int main() {
-    Martina
     string s; cin>>s;
     if(s[0]!=s[1] && s[0]!=s[2] && s[1]!=s[2]){
         cout<<"Yes";
