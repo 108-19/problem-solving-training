4d923dc26dc661079638aedeaefa77abc539e41f
108-19 <125561036+108-19@users.noreply.github.com>
2023-11-12 01:30:47 +0200

    87A.cpp

--- atcoder/phase1.1/2018-2019/87A.cpp
+++ atcoder/phase1.1/2018-2019/87A.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
-#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 int main() {
-    Martina
-        int x,a,b,c,d,e,rem; cin>>x>>a>>b;
-        c=x-a;
-        d=c/b;
-        e=d*b;
-        rem=c-e;
-        cout<<rem;
-          return 0;
-        }
+    int x,a,b,c,d,e,rem;
+    cin>>x>>a>>b;
+    c=x-a;
+    d=c/b;
+    e=d*b;
+    rem=c-e;
+    cout<<rem;
+    return 0;
+}
