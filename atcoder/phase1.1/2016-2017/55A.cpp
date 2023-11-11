caf06312dcb411d0c64cd18483aa8b783b4331e9
108-19 <125561036+108-19@users.noreply.github.com>
2023-11-12 00:50:15 +0200

    55A.cpp

--- atcoder/phase1.1/2016-2017/55A.cpp
+++ atcoder/phase1.1/2016-2017/55A.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
-#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 int main() {
-    Martina
-       int N; cin>>N;
-           if(N>=15){
-               cout<<N*800-N/15*200;
-           }
-           else {
-               cout<<N*800;
-           }
-               return 0;
-        }
+    int N;
+    cin>>N;
+    if(N>=15){
+        cout<<N*800-N/15*200;
+    }
+    else {
+        cout<<N*800;
+    }
+    return 0;
+}
