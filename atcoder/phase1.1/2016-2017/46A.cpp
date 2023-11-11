b0fc1495befee02dec787ecd40d232b28d2c9912
108-19 <125561036+108-19@users.noreply.github.com>
2023-11-12 00:23:22 +0200

    46A.cpp

--- atcoder/phase1.1/2016-2017/46A.cpp
+++ atcoder/phase1.1/2016-2017/46A.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-int a,b,c;
-cin>>a>>b>>c;
-if (a==b && b==c){
-    cout<<1;
-}
-else if (a==b || a==c || b==c){
-    cout<<2;
-}
-else if(a!=b && a!=c && b!=c){
-    cout<<3;
-}
-return 0;
-}
+    int a,b,c;
+    cin>>a>>b>>c;
+    if (a==b && b==c){
+        cout<<1;
+    }
+    else if (a==b || a==c || b==c){
+        cout<<2;
+    }
+    else if(a!=b && a!=c && b!=c){
+        cout<<3;
+    }
+    return 0;
+    }
