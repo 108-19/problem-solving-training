f33d618158d8575ce3a4c0149a668dfd93258681
108-19 <125561036+108-19@users.noreply.github.com>
2023-11-12 01:02:54 +0200

    58A.cpp

--- atcoder/phase1.1/2016-2017/58A.cpp
+++ atcoder/phase1.1/2016-2017/58A.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
-#define Martina ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 int main() {
-    Martina
-           int a,b,c; cin>>a>>b>>c;
-           if(b-a==c-b){
-               cout<<"Yes";
-           }
-           else {
-               cout<<"No";
-           }
-            return 0;
-        }
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(b-a==c-b){
+        cout<<"Yes";
+    }
+    else {
+        cout<<"No";
+    }
+    return 0;
+}
