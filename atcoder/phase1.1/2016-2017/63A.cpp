4d21f5f054e0997d051c77a96bfbdd29264d43e3
108-19 <125561036+108-19@users.noreply.github.com>
2023-11-12 01:08:45 +0200

    63A.cpp

--- atcoder/phase1.1/2016-2017/63A.cpp
+++ atcoder/phase1.1/2016-2017/63A.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-        int a,b; 
-        cin>>a>>b;
-        if (a + b < 10) {
-            cout << a + b;
-        }
-        else {
-            printf("error");
-        }
-    
-        return 0;
+    int a,b;
+    cin>>a>>b;
+    if (a + b < 10) {
+        cout << a + b;
     }
+    else {
+        cout<<"error"<<endl;
+    }
+    return 0;
+}
