b71aec310ca0df0dc38fb0cc79ef191252f3d6c1
108-19 <125561036+108-19@users.noreply.github.com>
2023-11-13 20:15:23 +0200

    97A.cpp

---
+++ atcoder/phase1.1/2018-2019/97A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+if(abs(a-c)<=d){
+cout<<"Yes";
+}
+else if( abs(a-b)<=d && abs(b-c)<=d){
+cout<<"Yes";
+}
+else {
+cout<<"No";
+}
+
+    return 0;
+}
