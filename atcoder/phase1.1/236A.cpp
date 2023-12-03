299e7e4037b893c358e5d8e1b27fd7cb967f8f9b
108-19 <125561036+108-19@users.noreply.github.com>
2023-12-03 07:28:03 +0200

    Create 236A.cpp

---
+++ atcoder/phase1.1/236A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string S;
+    int a, b;
+    cin >> S >> a >> b;
+    swap(S[a-1], S[b-1]);
+    cout << S << endl;
+    return 0;
+}
