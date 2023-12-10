c78d5de2d62e3328c329c61941d57781c633c8ef
108-19 <125561036+108-19@users.noreply.github.com>
2023-12-10 18:59:51 +0200

    65A.cpp

---
+++ atcoder/phase1.1/65A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int X,A,B; 
+  cin>>X>>A>>B;
+   if(B-A<=0){
+     cout<<"delicious"<<endl;
+   }
+  else if(B-A<=X) {
+    cout<<"safe"<<endl;
+  }
+  else if(B-A>X) {
+    cout<<"dangerous"<<endl;
+  }
+return 0;
+  }
