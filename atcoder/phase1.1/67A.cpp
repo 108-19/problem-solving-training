d5626a460365d8cbbd3b9958c7af1a9cec967df4
108-19 <125561036+108-19@users.noreply.github.com>
2023-12-10 18:56:02 +0200

    67A.cpp

---
+++ atcoder/phase1.1/67A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cin >>a>>b;
+	if(a%3==0){
+		cout << "Possible" << endl;
+	}
+  else if(b%3==0){
+		cout  << "Possible" << endl;
+	}
+  else if((a+b)%3==0){
+		cout << "Possible" << endl;
+	}
+	else{
+    cout << "Impossible" << endl;
+  }
+	return 0;
+}
