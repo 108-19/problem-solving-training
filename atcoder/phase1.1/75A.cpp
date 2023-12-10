65055a221090574927e9ffbd9efe5837acff76f7
108-19 <125561036+108-19@users.noreply.github.com>
2023-12-10 17:54:54 +0200

    75A.cpp

---
+++ atcoder/phase1.1/75A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+  int a,b,c;
+  cin>>a>>b>>c;
+  if(a==b){
+    cout<<c<<endl;
+  }
+  if(c==b){
+    cout<<a<<endl;
+   }
+  if(a==c){
+    cout<<b<<endl;
+  }
+return 0;
+}
