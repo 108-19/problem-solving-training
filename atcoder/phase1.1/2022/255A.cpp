bb6b334fa48ea042c5ea790c0fd4f3d64d59a8ff
108-19 <125561036+108-19@users.noreply.github.com>
2023-12-18 18:30:10 +0200

    255A.cpp

---
+++ atcoder/phase1.1/2022/255A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int x,y;
+  cin>>x>>y;
+  int a,b,c,d;
+  cin>>a>>b>>c>>d;
+  if(x==1){
+    if(y==1){
+      cout<<a<<endl;
+    }
+    else {
+      cout<<b<<endl;
+  }
+ if(y==1){
+      cout<<c<<endl;
+    }
+    else {
+      cout<<d<<endl;
+  }
+    return 0;
+}
