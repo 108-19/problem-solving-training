6aa593040d635cec1adba756f9b3c72f74e060cb
108-19 <125561036+108-19@users.noreply.github.com>
2023-12-18 15:49:33 +0200

    282A.cpp

---
+++ atcoder/phase1.1/282A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int K; cin >> K;
+    char now = 'A';
+    for(int i=0; i<K; i++){
+        cout << now;
+        now++;
+    }
+    cout << endl;
+    return 0;
+}
