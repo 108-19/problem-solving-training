4d39d80e3801328d027e7f536e92d8cade4074b8
108-19 <125561036+108-19@users.noreply.github.com>
2024-01-01 16:55:11 +0200

    208A.cpp

---
+++ atcoder/phase1.2/208A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+    int A,B;
+    cin>> A >> B;
+
+    if(B>=A && B<= 6*A)
+    {
+        cout<<"Yes";
+    }
+    else 
+    {
+        cout<<"No";
+    }
+
+    return 0;
+}
