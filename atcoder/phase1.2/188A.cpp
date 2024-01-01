131fd163b8e6fbfeb288994252fc5e031407f72d
108-19 <125561036+108-19@users.noreply.github.com>
2024-01-01 16:08:26 +0200

    188A.cpp

---
+++ atcoder/phase1.2/188A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x,y;
+    cin>>x>>y;
+    if(x < y){
+        if(x+3 > y){
+            cout<<"Yes";
+        }
+        else{
+            cout<<"No";
+        }
+    }
+    else if(y < x){
+        if(y+3 > x){
+            cout<<"Yes";
+        }
+        else{
+            cout<<"No";
+        }
+    }
+  return 0;
+    }
