f8759c419463d63a4f2b71193e5397a652a56eca
108-19 <125561036+108-19@users.noreply.github.com>
2024-02-29 17:30:32 +0200

    215A.cpp

---
+++ atcoder/phase1.2/2020-2021/215A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+   string s;
+   cin>>s;
+   if (s=="Hello,World!"){
+        cout<<"AC"<<"\n";
+   }
+   else{
+        cout<<"WA"<<"\n";
+   }
+
+    return 0;
+}
