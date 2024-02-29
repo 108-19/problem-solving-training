9966f6a7fd5185fcef566bc5d83651f78ef0b19c
108-19 <125561036+108-19@users.noreply.github.com>
2024-02-29 19:12:17 +0200

    331A.cpp

---
+++ atcoder/phase1.2/2020-2021/331A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int M,D;
+  cin >> M >> D;
+  int y,m,d;
+  cin >> y >> m >> d;
+  d++;
+  if (d > D){
+    d = 1;
+    m++;
+  }
+  if (m > M){
+    m = 1;
+    y++;
+  }
+  cout << y << ' ' << m << ' ' << d << endl;
+  return 0;
+}
