073300bf6602218df85e7e58e48b5692a7fe8033
108-19 <125561036+108-19@users.noreply.github.com>
2024-02-29 17:39:22 +0200

    224A.cpp

---
+++ atcoder/phase1.2/2020-2021/224A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<cmath>
+#include <string>
+using namespace std;
+
+int main() {
+  string s;
+  cin>>s;
+ char b=s[s.length()-1];
+ if(b=='r')cout<<"er";
+  else cout<<"ist";
+    return 0;
+}
