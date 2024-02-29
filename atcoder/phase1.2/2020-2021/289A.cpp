a664d448dd1eaffaf8f4c32ae7a37f0fd37ec2ff
108-19 <125561036+108-19@users.noreply.github.com>
2024-02-29 17:55:56 +0200

    289A.cpp

---
+++ atcoder/phase1.2/2020-2021/289A.cpp
@@ -0,0 +1,17 @@
+    #include <bits/stdc++.h>
+
+    using namespace std;
+
+    int main ()
+{
+    string s;
+    cin >> s;
+
+    for(int i = 0; i<s.size(); i++)
+   {
+    if(s[i]=='1'){s[i] = '0';}
+    else if (s[i]=='0'){s[i] = '1';}
+   }
+   cout << s;
+    return 0;
+}
