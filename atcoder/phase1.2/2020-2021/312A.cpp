7fe0dc187ea886cac1a16ec80e096f25494079d9
108-19 <125561036+108-19@users.noreply.github.com>
2024-02-29 19:05:18 +0200

    312A.cpp

---
+++ atcoder/phase1.2/2020-2021/312A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;
+  cin >> s;
+  if(s == "GBD")
+    cout << "Yes" << endl;
+  else if(s == "ACE") 
+    cout << "Yes" << endl;
+  else if(s == "BDF") 
+    cout << "Yes" << endl;
+  else if(s == "CEG") 
+    cout << "Yes" << endl;
+  else if(s == "DFA")
+    cout << "Yes" << endl;
+  else if(s == "EGB") 
+    cout << "Yes" << endl;
+  else if(s == "FAC")
+    cout << "Yes" << endl;
+  else  cout << "No" << endl;
+  return 0;
+}
