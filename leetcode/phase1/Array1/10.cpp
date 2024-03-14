5a8107f2371c2169edbba5e02106186e015009f7
108-19 <125561036+108-19@users.noreply.github.com>
2024-03-14 22:59:27 +0200

    10.cpp

--- leetcode/phase1/Array1/10.cpp
+++ leetcode/phase1/Array1/10.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+         int n = arr.size();
+        int sum = 0;
 
+        for (int j = 0; j< n; ++j) {
+            for (int length = 1; j + length <= n; length += 2) {
+                int k = j + length - 1; 
+                
+                for (int i = j; i <= k; ++i) {
+                    sum += arr[i];
+                }
+            }
+        }
+
+        return sum;
+    }
+};
