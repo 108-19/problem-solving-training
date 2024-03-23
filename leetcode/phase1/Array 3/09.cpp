e7ee328d3b9113bd5ea3fcfa1f1f71cf369804b7
Martina Samuel <125561036+108-19@users.noreply.github.com>
2024-03-23 07:29:12 +0200

    09.cpp

---
+++ leetcode/phase1/Array 3/09.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int arrSize = arr.size();
+
+        for (int i = 0; i < arrSize - 2; i++) {
+            if (arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
